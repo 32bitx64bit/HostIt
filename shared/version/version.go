44681dcac501f422d98f8050127edf8ed6517b90
32bitx64bit <gavinx2031@triro.net>
2026-02-15 01:35:20 -0600

    bump

--- shared/version/version.go
+++ shared/version/version.go
@@ -8,7 +8,7 @@ import (
 
 // Current is the currently running HostIt/Playit-prototype version.
 // Keep this in sync with your GitHub release tags.
-const Current = "1.8.6"
+const Current = "1.9.0"
 
 type Version struct {
 	Major int
